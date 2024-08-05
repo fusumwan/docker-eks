9fc28020c4f805d798011417e1b73170a45a5621
MSI\timot <timothyfuapple@gmail.com>
2024-08-05 15:14:14 +0930

    updated

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const ENV = 'DEV';
 const app = express();
 app.get('/', (req, res) => {
   res.statusCode = 200;
-  const msg = 'Timothy Java Channel';
+  const msg = '[Timothy Java Channel]';
   res.send(getPage(msg));
 });
 
